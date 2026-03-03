64950b5168c58c519b2b736dd8b3b7a7ab1ff98a
MCODE OPS <modelcode-bot@users.noreply.github.com>
2026-03-03 14:04:24 +0000

    SRC spec validation (milestone 3) Attempt number: 2

--- .mcode/tests/functional_testing/m3-0852cc0d-test_protocol_spec.py
+++ .mcode/tests/functional_testing/m3-0852cc0d-test_protocol_spec.py
@@ -9,7 +9,7 @@ This script supports two modes:
 1. SRC Validation: Tests endpoints and captures responses (no expected_response)
 2. DST Contract Validation: Tests endpoints and validates responses match expected (has expected_response)
 
-Generated at: 2026-03-03T14:01:11.226366+00:00
+Generated at: 2026-03-03T14:04:23.295252+00:00
 Project: flask-rest-api-jwt
 Milestone: 3
 """
